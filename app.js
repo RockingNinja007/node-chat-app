e402845e2e40998dd63e24ca4b78f320e3aad4d4
vikas <vikas.ratre@gmail.com>
2018-08-06 14:01:44 +0530

    random guess

--- app.js
+++ app.js
@@ -26,7 +26,9 @@ var socketUsers = [];
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP;
  
-server.listen(5000);
+server.listen(server_port, server_ip_address, function () {
+  //console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
 
 // setting up session middleware
 app.use(session({secret: 'secretdata'}));
