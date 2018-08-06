53558e8714c0d94b5d11952653045b764a345173
vikas <vikas.ratre@gmail.com>
2018-08-06 12:56:35 +0530

    going to default

--- app.js
+++ app.js
@@ -26,9 +26,7 @@ var socketUsers = [];
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP;
  
-server.listen(server_port, server_ip_address, function () {
-  //console.log( "Listening on " + server_ip_address + ", port " + server_port )
-});
+server.listen(5000);
 
 // setting up session middleware
 app.use(session({secret: 'secretdata'}));
