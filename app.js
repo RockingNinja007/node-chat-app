775b8418d47e449a151f8f6aca0fa67c1272cb7f
vikas <vikas.ratre@gmail.com>
2018-08-08 12:02:13 +0530

    personal chat resolved

--- app.js
+++ app.js
@@ -25,9 +25,13 @@ var socketUsers = [];
 //listning the server
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP;
- 
-server.listen(5000);
 
+server.listen(server_port, server_ip_address, function () {
+    //console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
+ /* 
+server.listen(5000);
+ */
 // setting up session middleware
 app.use(session({secret: 'secretdata'}));
 
@@ -71,6 +75,7 @@ app.post("/afterRegstration",(req,res)=>{
    // console.log("working");
     req.on('data', (data)=>{
         var dat = ""+data;
+
         var username = JSON.parseLookup(JSON.parseLookup(dat, '&')[0], '=')[1];
         var password = JSON.parseLookup(JSON.parseLookup(dat, '&')[1], '=')[1];
         
